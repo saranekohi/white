2dff9d1f1e6dd7f3ea289b2759762441306d5758
HighRiskShop.COM <support@highriskshop.com>
2024-10-17 21:03:18 +0300

    Custom error pages

--- hidden-affiliate-address-version/worker.js
+++ hidden-affiliate-address-version/worker.js
@@ -10,19 +10,30 @@ async function handleRequest(request) {
   const url = new URL(request.url);
   url.hostname = new URL(targetUrl).hostname;
 
-  // Add the affiliate parameter to the URL where you can replace this Polygon wallet with your own affiliate wallet hidden from your merchants in the address bar
+  // Check if the path contains "wallet.php" and replace it with "affiliate.php"
+  if (url.pathname.includes('wallet.php')) {
+    url.pathname = url.pathname.replace('wallet.php', 'affiliate.php');
+  }
+
+  // Add the affiliate parameter to the URL while preserving the existing search params
   url.search += (url.search ? '&' : '') + 'affiliate=0x505e71695E9bc45943c58adEC1650577BcA68fD9';
 
+  // Create a modified request with the updated URL
   const modifiedRequest = new Request(url.toString(), request);
-  
+
   // Make a request to the target URL
   const response = await fetch(modifiedRequest);
-  
+
+  // If the response status code is in the 40X range, redirect to custom error page https://www.example.com/error
+  if (response.status >= 400 && response.status < 500) {
+    return Response.redirect('https://www.example.com/error', 302);
+  }
+
   // Clone the response to modify headers
   const modifiedResponse = new Response(response.body, response);
-  
+
   // Set headers to cloak the origin
   modifiedResponse.headers.set('Access-Control-Allow-Origin', '*');
-  
+
   return modifiedResponse;
 }
